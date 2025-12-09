9b8fa946fbb680d14bd0565bd284f21bab06cc1f
WillianRoosa <willian_roosa@icloud.com>
2025-12-09 19:22:16 -0300

    primeiro commit - atividade EdN

---
+++ atividade01.py
@@ -0,0 +1,22 @@
+# 1. Programa de Saudação
+print('Olá Mundo!')
+
+# 2. Calculadora de Soma
+num1 = 92
+num2 = 20
+soma = num1 + num2
+print('A soma de', num1, 'e', num2, 'é:', soma)
+
+# 3. Calculadora de Volume
+comprimento = 5
+largura = 3
+altura = 4
+volume = comprimento * largura * altura
+print('O volume da caixa é:', volume)
+
+# 4. Calculadora de Preço Total
+nome_produto = 'Caneta'
+preco_unitario = 2.50
+quantidade = int(input('Digite a quantidade de ' + nome_produto + ' que deseja comprar: '))
+preco_total = preco_unitario * quantidade
+print('O preço total para', quantidade, nome_produto + '(s) é: R$', preco_total)
